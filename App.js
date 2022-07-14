9f286b8cabe8632ede51eae63be7b45e502c4bf3
Sourav Mishra <sourav.mishra@squbix.com>
2022-07-14 12:58:28 +0530

    Finger Push

--- App.js
+++ App.js
@@ -7,13 +7,18 @@
  */
 
 import React from 'react';
-import Logo from './assets/Logo.svg'
+import Logo from './assets/Logo.svg';
+import Finger from './assets/Finger.svg';
+import MetaLogo from './assets/MetaLogo.svg';
+import SplashBlob from './assets/Splash_Blob.svg';
 import {
+  ImageBackground,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  Image,
   useColorScheme,
   View,
 } from 'react-native';
@@ -26,17 +31,40 @@ const App = () => {
     }}>
     </SafeAreaView>
     <View style={styles.container}>
-      <Logo style={{
-        marginTop: 40,
-        marginBottom: 40
+      <MetaLogo style={{
+        alignSelf: 'flex-start',
+        marginLeft: 10
       }}
-      height={300} 
-      width={300} 
+        height={50} 
+        width={120}
       />
+      <View style={{
+        borderWidth: 2,
+        border: 'solid',
+        borderColor: 'white',
+        borderRadius: 180,
+        marginTop: 100,
+        marginBottom: 70,
+        padding: 20
+      }} 
       
-      <Text style={styles.text}>Self</Text>
-      <Text style={styles.text}>Sovereign</Text>
-      <Text style={styles.text}>Identity</Text>
+      > 
+        <Logo height={300} width={300} 
+        />
+       </View> 
+      <View style={{
+        flex: 1,
+        flexDirection: 'row'
+      }}>
+          {/* <Finger height={150} width={150}/> */}
+        <Image style={{
+          height:160,
+          width: 160
+        }}
+          source={require('./assets/Finger.png')}
+        />
+          <Text style={styles.text}>SELF{'\n'}SOVEREIGN{'\n'}IDENTITY</Text>
+      </View>
     </View>
     </>
   );
@@ -51,10 +79,7 @@ const styles = StyleSheet.create({
   text: {
     color: '#e6e6e6',
     fontSize: 40,
-    // fontWeight: '100',
-    letterSpacing: 1,
-    alignItems: 'center',
-    fontFamily: 'Roboto-Black'
+    fontWeight: '600',
   },
 })
 
